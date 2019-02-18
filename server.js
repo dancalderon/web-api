323b908a0e7e5ef7169a81499aa3ee573aa27b2e
ofaniel <dancv@outlook.es>
2019-02-17 20:26:27 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import hbs from "hbs";
+import fs from "fs"
+const app = express()
+
+
+hbs.registerPartials(`${__dirname}/views/partials`) //establishes the partials folder
+app.set('view engine', 'hbs'); //establishes hbs as the view engine
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '/n', err => {
+    if (err) console.log('Unable to append to server.log');
+  })
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', text => text.toUpperCase())
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcome: 'Welcome to this site',
+  })
+}
+)
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  })
+}
+)
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Website unavaibla'
+  })
+})
+
+app.listen(3000);
