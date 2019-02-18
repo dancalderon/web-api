fbab88787a7b3b5b40039d2d27c252d49a593646
ofaniel <dancv@outlook.es>
2019-02-18 13:42:08 -0500

    projects works

--- server.js
+++ server.js
@@ -39,18 +39,21 @@ app.get('/about', (req, res) => {
     pageTitle: 'About page',
   })
 }
-)
-app.get('/portfolio', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'Portfolio page',
+);
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
   })
 }
-)
+);
+
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Website unavaibla'
   })
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is up in port ${port}`)
